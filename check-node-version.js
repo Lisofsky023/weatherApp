fcbfd50703c713844fc9c59877a9df5fa1c0d4e5
Lisofsky023 <lisa.2023.ge@gmail.com>
2023-09-29 14:37:39 +0400

    change node -v

--- check-node-version.js
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-
-const requiredVersion = readFileSync('.nvmrc', 'utf-8').trim();
-const currentVersion = process.version.replace('v', '');
-
-if (requiredVersion !== currentVersion) {
-  console.error(`⚠️  The project requires Node.js ${requiredVersion} but you are using ${currentVersion}. Please switch versions using NVM or install the required version.`);
-  process.exit(1);
-}
