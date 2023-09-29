28a2316f8e52895da28374d6eb0ac5943e8cafcf
Lisofsky023 <lisa.2023.ge@gmail.com>
2023-09-29 17:22:24 +0400

    this is the end честное слово

--- src/components/CityItem/CityItem.tsx
+++ src/components/CityItem/CityItem.tsx
@@ -1,5 +1,37 @@
-import React from 'react';
+// import React from 'react';
+// import { City } from '../../services/weatherTypes';
+
+// interface CityItemProps {
+//   city: City;
+//   onRemove: (name: string) => void;
+// }
+
+// const CityItem: React.FC<CityItemProps> = ({ city, onRemove }) => {
+//   return (
+//     <li className='cities__item' key={city.id}>
+//       {city.weatherData && (
+//         <div className='cities__item__weather-data'>
+//           <div className="cities__name-temp">
+//             <p className='cities__item__name'>{city.name}</p>
+//             <p className='cities__temp'>{city.weatherData.current.temp_c}°C</p>
+//             <img src={city.weatherData.current.condition.icon} alt={city.weatherData.current.condition.text} />
+//           </div>
+//           <div className="cities__data">
+//             {/* <p>{city.weatherData.current.condition.text}</p> */}
+//             <p>Влажность: {city.weatherData.current.humidity}%</p>
+//             <p>Ветер: {city.weatherData.current.wind_kph} км/ч</p>
+//           </div>
+//         </div>
+//       )}
+//       <button className='cities__item__remove-button' onClick={() => onRemove(city.name)}>Delete</button>
+//     </li>
+//   );
+// }
+// export default CityItem;
+
+import React, { useEffect, useState } from 'react';
 import { City } from '../../services/weatherTypes';
+import { useSwipeable } from 'react-swipeable';
 
 interface CityItemProps {
   city: City;
@@ -7,8 +39,26 @@ interface CityItemProps {
 }
 
 const CityItem: React.FC<CityItemProps> = ({ city, onRemove }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handlers = isMobile ? useSwipeable({
+    onSwipedLeft: () => onRemove(city.name),
+    trackMouse: true
+  }) : {};
+
   return (
-    <li className='cities__item' key={city.id}>
+    <li className='cities__item' key={city.id} {...handlers}>
       {city.weatherData && (
         <div className='cities__item__weather-data'>
           <div className="cities__name-temp">
@@ -23,7 +73,9 @@ const CityItem: React.FC<CityItemProps> = ({ city, onRemove }) => {
           </div>
         </div>
       )}
-      <button className='cities__item__remove-button' onClick={() => onRemove(city.name)}>Delete</button>
+      {!isMobile && (
+        <button className='cities__item__remove-button' onClick={() => onRemove(city.name)}>Delete</button>
+      )}
     </li>
   );
 }
