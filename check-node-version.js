bbd4fb5f7bf0bd22990bca0324c86974f816ccb0
Lisofsky023 <lisa.2023.ge@gmail.com>
2023-09-29 14:07:52 +0400

    add node i

---
+++ check-node-version.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const requiredVersion = fs.readFileSync('.nvmrc', 'utf-8').trim();
+const currentVersion = process.version.replace('v', '');
+
+if (requiredVersion !== currentVersion) {
+  console.error(`⚠️  The project requires Node.js ${requiredVersion} but you are using ${currentVersion}. Please switch versions using NVM or install the required version.`);
+  process.exit(1);
+}
