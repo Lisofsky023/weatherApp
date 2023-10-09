163dc57227055a314f5eceffe3cd2d24e2c687c2
Lisofsky023 <lisa.2023.ge@gmail.com>
2023-10-09 13:04:56 +0400

    fix error catch throw

--- src/services/WeatherService.tsx
+++ src/services/WeatherService.tsx
@@ -2,33 +2,48 @@ import { WeatherData, Suggestion } from '../services/weatherTypes';
 
 const API_KEY = '91e55b8e09544d938b7120222232509';
 
-// Функция для получения текущих погодных данных для указанного города
 export const fetchWeatherData = async (cityName: string): Promise<WeatherData> => {
     try {
-        // Запрос к API для получения погодных данных
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}`);
+        
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || 'Error fetching weather data');
+        }
+
         const data: WeatherData = await response.json();
         return data;
+
     } catch (error) {
-        throw error;
+        if (error instanceof Error) {
+            throw new Error(`Failed to fetch weather data: ${error.message}`);
+        } else {
+            throw new Error('Failed to fetch weather data');
+        }
     }
 };
 
-// Функция для получения списка предложений на основе запроса
 export const fetchSuggestions = async (query: string): Promise<Suggestion[]> => {
     try {
-        // Если запрос пустой, возвращаем пустой список
         if (!query) {
             return [];
         }
-        // Запрос к API для получения предложений
+
         const response = await fetch(`https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${query}`);
+        
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || 'Error fetching suggestions');
+        }
+
         const suggestions: Suggestion[] = await response.json();
         return suggestions;
+
     } catch (error) {
-        throw error;
+        if (error instanceof Error) {
+            throw new Error(`Failed to fetch suggestions: ${error.message}`);
+        } else {
+            throw new Error('Failed to fetch suggestions');
+        }
     }
 };
-
-
-
