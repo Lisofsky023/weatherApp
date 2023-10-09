79a85349144fa5dc6491feaff90f138e386fc112
Lisofsky023 <lisa.2023.ge@gmail.com>
2023-10-09 13:14:49 +0400

    useDebounce add Generic

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,23 +1,18 @@
 import { useState, useEffect } from 'react';
-// Хук useDebounce позволяет "задерживать" обновление значения, что полезно, например, для уменьшения 
-// числа запросов при вводе текста в поле поиска.
-export function useDebounce(value: any, delay: number) {
-  // Состояние, которое будет обновляться с задержкой
-  const [debouncedValue, setDebouncedValue] = useState(value);
+
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    // Устанавливаем таймер, который будет вызываться после времени, указанного в delay
     const handler = setTimeout(() => {
-      // После истечения задержки обновляем состояние
       setDebouncedValue(value);
     }, delay);
 
-    // Эта функция будет вызвана перед следующим эффектом или при размонтировании компонента
-    // Она отменяет таймер, если значение или задержка изменяется
     return () => {
       clearTimeout(handler);
     };
-  }, [value, delay]);  // Эффект будет перезапущен, если value или delay изменится
+  }, [value, delay]);
 
-  return debouncedValue; // Возвращаем значение с задержкой
+  return debouncedValue;
 }
+
